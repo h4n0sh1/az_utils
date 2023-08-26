1301fa6d6fc9b194a239e2f79d531716491f0e75
h4wk <hanchi.oth@gmail.com>
2023-08-26 22:37:38 +0200

    max_cert

---
+++ gen_cert.ps1
@@ -0,0 +1,15 @@
+﻿<#
+    Windows : Generate cer/pfx certificates 
+#>
+param(
+    [string]$certname = "Test",
+    [string]$type = "cer" # |pfx
+)
+$cert = New-SelfSignedCertificate -Subject "CN=$certname" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+
+if($type -eq "cer"){
+    Export-Certificate -Cert $cert -FilePath "$PSScriptRoot\$certname.cer" 
+}else{
+    Export-PfxCertificate -FilePath "$PSScriptRoot\$certname.pfx" -Password ""
+}
+    
---
+++ getMaxCertificate.ps1
@@ -0,0 +1,24 @@
+﻿<#
+    Az - App Reg : Check max number of certificate per app. reg  
+#>
+param(
+    [string]$appRegName = "Test-MaxCert"
+)  
+$i=1
+$err=0
+Connect-AzureAD
+do{
+    try {
+        Write-Host "Setting Key n°$i"  
+        & "$PSScriptRoot\gen_cert.ps1" $appRegName
+        $certFile = "$PSScriptRoot\$appRegName.cer"
+        $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate($certFile)
+        $keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+        $azureADAppReg = Get-AzureADApplication -Filter "DisplayName eq '$appRegName'"
+        $key_cred = New-AzureADApplicationKeyCredential -ObjectId $azureADAppReg.ObjectId -Type AsymmetricX509Cert -Usage Verify -Value $keyValue
+        Remove-Item -Path "$PSScriptRoot\$appRegName.cer"  
+    }
+    catch { $err=1 }
+    $i+=1
+}while(!$err)
+
