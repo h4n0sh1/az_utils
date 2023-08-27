f2c7cb00e8950542f1c7eac9736f0042995f6a26
h4wk <hanchi.oth@gmail.com>
2023-08-27 22:06:30 +0200

    cls

---
+++ cleanAppRegSecrets.ps1
@@ -0,0 +1,20 @@
+﻿<#
+    Az - App Reg : Clean all secrets from app. registration
+#>
+param(
+    [string]$appRegName = "app_reg_name"
+) 
+
+
+#Connect-AzAccount
+$azureADAppReg = Get-AzureADApplication -Filter "DisplayName eq '$appRegName'"
+$secret_list = @()
+
+try {
+	"Cleaning ..."
+	Set-AzureADApplication -ObjectID $azureADAppReg.ObjectId -PasswordCredentials $secret_list 
+}
+catch { 
+	"Error ..."
+}
+    
